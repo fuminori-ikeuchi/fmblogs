60aa201fdee83f7303034b0bca91001627cb5f53
fuminori.ikeuchi <fuminori.7127.lv19@icloud.com>
2020-11-23 14:38:12 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
   
+  get 'relationships/create'
+  get 'relationships/destroy'
   root to: 'users#index'
   
   get 'signup', to: 'users#new'
@@ -9,6 +11,13 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  resources :users, only: [:index, :show, :create, :edit, :update]
+  resources :users, only: [:index, :show, :create, :edit, :update] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
+  
   resources :posts, only: [:show, :create, :edit, :update, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
